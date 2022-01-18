4ab37df9eaa205655c0a7a98856227213eea960e
Himayan Debnath <43062690+himayand21@users.noreply.github.com>
2022-01-18 17:15:44 +0530

    solution: first-thumb-start

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,6 +64,15 @@ Instructions:
 
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+    .then(function (response) {
+      addSearchHeader(response.query);
+      return getJSON(response.results[0])
+    }).catch(function() {
+      throw new Error('Search Error');
+    }).then(createPlanetThumb).catch(function(error) {
+      addSearchHeader('unknown');
+      console.error(error);
+    });
   });
 })(document);
