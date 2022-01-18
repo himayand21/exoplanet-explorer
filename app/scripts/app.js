7a110b48564b51486586df769143007ee5ec85af
Himayan Debnath <43062690+himayand21@users.noreply.github.com>
2022-01-18 18:00:27 +0530

    solution: foreach-start

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,9 +64,15 @@ Instructions:
      */
     getJSON('../data/earth-like-results.json')
     .then(function(response) {
+      var sequence = Promise.resolve();
       response.results.forEach(function(url) {
-        getJSON(url).then(createPlanetThumb);
+        sequence = sequence.then(function() {
+          return getJSON(url)
+        }).then(createPlanetThumb);
       });
+    })
+    .catch(function(error) {
+      console.error(error);
     });
   });
 })(document);
